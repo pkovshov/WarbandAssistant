e5cb4ac5c340449874a49c975b5242ec477124a1
Pavel Kovshov <pavel.kovshov@gmail.com>
2025-07-04 21:22:13 +0300

    Add test for DialogScreenRelationDataset

--- tests/dialog_screen_datasets_test.py
+++ tests/dialog_screen_datasets_test.py
@@ -7,6 +7,7 @@ import numpy as np
 import path_conf
 from wa_types import Box, Resolution
 from wa_datasets.DialogScreen.DialogTitleDataset import DialogTitleDataset
+from wa_datasets.DialogScreen.DialogRelationDataset import DialogRelationDataset
 
 # Create the output directory if it doesn't exist
 os.makedirs(path_conf.test_output, exist_ok=True)
@@ -20,7 +21,8 @@ for item in os.listdir(path_conf.test_output):
         os.remove(item_path)
 
 # Copy the dialog screen datasets to test_output
-for dataset in [DialogTitleDataset.NAME]:
+for dataset in [DialogTitleDataset.NAME,
+                DialogRelationDataset.NAME]:
     src_path = path.join(path_conf.datasets, dataset)
     dst_path = path.join(path_conf.test_output, dataset)
     shutil.copytree(src_path, dst_path)
@@ -28,7 +30,7 @@ for dataset in [DialogTitleDataset.NAME]:
 # Override the datasets path for testing in a controlled environment
 path_conf.datasets = path_conf.test_output
 
-def test_dialog_title_dataset():
+def test_dialog_titles_dataset():
     dialog_title_dataset_path = path.join(path_conf.datasets,
                                           DialogTitleDataset.NAME)
     files_count_before = len(os.listdir(dialog_title_dataset_path))
@@ -44,4 +46,21 @@ def test_dialog_title_dataset():
                 title_fuzzy_score=None,
                 title_keys=())
     files_count_after = len(os.listdir(dialog_title_dataset_path))
+    assert files_count_after == files_count_before + 2
+
+
+def test_dialog_relations_dataset():
+    dialog_relation_dataset_path = path.join(path_conf.datasets,
+                                             DialogRelationDataset.NAME)
+    files_count_before = len(os.listdir(dialog_relation_dataset_path))
+    width, height = 400, 200
+    dataset = DialogRelationDataset(resolution=Resolution(width, height),
+                                    crop=Box(0, 0, 50, 50),
+                                    language="en")
+    screenshot = np.zeros((height, width, 3), dtype=np.uint8)
+    dataset.add(screenshot=screenshot,
+                screen_sample_matches=True,
+                relation_ocr="16",
+                relation=16)
+    files_count_after = len(os.listdir(dialog_relation_dataset_path))
     assert files_count_after == files_count_before + 2
