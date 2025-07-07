a80240f15c9d1e46a96bed9510d62ccfeb02f031
Pavel Kovshov <pavel.kovshov@gmail.com>
2025-07-08 01:11:08 +0300

    Change sample box for map screen. Cause: Game Log overlaps previous sample box

--- src/wa_screen_manager/MapScreen/map_screen_config.py
+++ src/wa_screen_manager/MapScreen/map_screen_config.py
@@ -5,5 +5,5 @@ from wa_types import Box
 
 map_screen_sample_img_path = path.join(path_conf.samples,
                                        "map_screen_sample.png")
-map_screen_sample_boxes = Box(0, 1008, 42, 1080),
+map_screen_sample_boxes = Box(1868, 1008, 1920, 1080),
 calendar_box = Box(1540, 1016, 1920, 1080)
