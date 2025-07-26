03c9312c871f643a41fcc7b7c374bbd69e850167
Pavel Kovshov <pavel.kovshov@gmail.com>
2025-07-26 04:02:17 +0300

    Add tox.py script that runs tox three time for each typechecher and collect logs. Collect tox logs.

---
+++ tox.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+import os
+from os import path
+import subprocess
+
+import pexpect
+
+
+log_dir = path.abspath("toxlogs")
+
+typecheckers = {
+    "BEAR": "beartype",
+    "GUARD": "guardtype",
+    "NONE": "nonetype"
+}
+
+first = True
+
+for i in range(1, 4):
+    for checker, checker_name in typecheckers.items():
+
+        logfile_path = path.join(log_dir, f"tox-{checker_name}-{i}.log")
+
+        if not first:
+            print()
+            print()
+        first = False
+        print(f"$ TYPECHECKER={checker} tox (run {i})")
+
+        env = os.environ.copy()
+        env["TYPECHECKER"] = checker
+        with open(logfile_path, "w") as logfile:
+            # use ansi2txt unix program to remove ansi escape characters
+            ansi2txt = subprocess.Popen(
+                ['ansi2txt'],
+                stdin=subprocess.PIPE,
+                stdout=logfile,
+                stderr=subprocess.PIPE,
+                text=True
+            )
+            tox = pexpect.spawn("tox",
+                                env=env)
+            while True:
+                match = tox.expect([pexpect.EOF, '\r\n'], timeout=None)
+                line = tox.before.decode('utf-8')
+                print(line, flush=True)
+                print(line, flush=True, file=ansi2txt.stdin)
+                if match == 0:
+                    break
+            ansi2txt.communicate()
--- tox.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/sh
-tox | tee tox-1.log ; tox | tee tox-2.log; tox | tee tox-3.log
